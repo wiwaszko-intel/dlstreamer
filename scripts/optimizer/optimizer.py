199ed99c19ba96431337d42dc51894b2db33962f
pbartosik <pawelbrtsk@gmail.com>
2026-01-20 16:00:51 +0100

    Optimizer refactor (#532)
    * first commit
    
    * fixed problem with old pipelines
    
    * removed debug print
    
    * Updated main logic comment
    
    * Update scripts/optimizer/optimizer.py
    
    Co-authored-by: Olena Onyshchenko <olena.onyshchenko@intel.com>
    
    * added a test for pre-processed pipelines
    
    * added a check for tee element
    
    * lint fixes
    
    * more lint fixes
    
    * more lint fixes
    
    * updated license headers
    
    ---------
    
    Co-authored-by: Olena Onyshchenko <olena.onyshchenko@intel.com>

--- scripts/optimizer/optimizer.py
+++ scripts/optimizer/optimizer.py
@@ -1,15 +1,16 @@
 # ==============================================================================
-# Copyright (C) 2025-2025 Intel Corporation
+# Copyright (C) 2025-2026 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 # ==============================================================================
-from preprocess import preprocess_pipeline
-from processors.inference import add_device_suggestions, add_batch_suggestions, add_nireq_suggestions, parse_element_parameters # pylint: disable=line-too-long
-
 import time
 import logging
 import itertools
 import os
+import re
+
+from preprocess import preprocess_pipeline
+from processors.inference import DeviceGenerator, BatchGenerator, NireqGenerator
 
 import gi
 gi.require_version("Gst", "1.0")
@@ -31,13 +32,16 @@ logger.info("GStreamer version: %d.%d.%d",
 # Steps of pipeline optimization:
 # 1. Measure the baseline pipeline's performace.
 # 2. Pre-process the pipeline to cover cases where we're certain of the best alternative.
-# 3. Prepare a set of processors providing alternatives for elements.
-# 3. Run the processors in sequence and test their effect on the pipeline.
-# 5. For every processor create a cartesian product of the suggestions
-#    and start running the combinations to measure performance.
+# 3. Prepare a set of generators providing alternatives for elements.
+# 4. Iterate over the generators
+# 5. Iterate over the suggestions from every processor
 # 6. Any time a better pipeline is found, save it and its performance information.
 # 7. Return the best discovered pipeline.
 def get_optimized_pipeline(pipeline, search_duration = 300, sample_duration = 10):
+    # Test for tee element presence
+    if re.search("[^a-zA-Z]tee[^a-zA-Z]", pipeline):
+        raise RuntimeError("Pipelines containing the tee element are currently not supported!")
+
     pipeline = pipeline.split("!")
 
     # Measure the performance of the original pipeline
@@ -51,58 +55,48 @@ def get_optimized_pipeline(pipeline, search_duration = 300, sample_duration = 10
 
     # Make pipeline definition portable across inference devices.
     # Replace elements with known better alternatives.
-    pipeline = "!".join(pipeline)
-    pipeline = preprocess_pipeline(pipeline)
-    pipeline = pipeline.split("!")
+    try:
+        preproc_pipeline = " ! ".join(pipeline)
+        preproc_pipeline = preprocess_pipeline(preproc_pipeline)
+        preproc_pipeline = preproc_pipeline.split(" ! ")
 
-    processors = [
-        add_device_suggestions,
-        add_batch_suggestions,
-        add_nireq_suggestions,
+        preproc_fps = sample_pipeline(preproc_pipeline, sample_duration)
+        if preproc_fps > fps:
+            fps = preproc_fps
+            pipeline = preproc_pipeline
+    except Exception:
+        logger.error("Pipeline pre-processing failed, using original pipeline instead")
+
+    generators = [
+        DeviceGenerator(),
+        BatchGenerator(),
+        NireqGenerator()
     ]
 
-    search_end_time = time.time() + search_duration
-    for processor in processors:
-        remaining_duration = search_end_time - time.time()
-        if search_end_time <= time.time():
-            break
+    best_pipeline = pipeline
+    best_fps = fps
+    start_time = time.time()
+    for generator in generators:
+        generator.init_pipeline(best_pipeline)
+        for pipeline in generator:
+            cur_time = time.time()
+            if cur_time - start_time > search_duration:
+                break
 
-        suggestions = prepare_suggestions(pipeline)
-        processor(suggestions)
-        pipeline, fps = explore_pipelines(suggestions, fps, remaining_duration, sample_duration)
+            try:
+                fps = sample_pipeline(pipeline, sample_duration)
+
+                if fps > best_fps:
+                    best_fps = fps
+                    best_pipeline = pipeline
+
+            except Exception as e:
+                logger.debug("Pipeline failed to start: %s", e)
 
     # Reconstruct the pipeline as a single string and return it.
-    return "!".join(pipeline), fps
+    return "!".join(best_pipeline), best_fps
 
 ##################################### Pipeline Running ############################################
-
-def explore_pipelines(suggestions, base_fps, search_duration, sample_duration):
-    start_time = time.time()
-    combinations = itertools.product(*suggestions)
-    # first element is the original pipeline, use it as baseline
-    best_pipeline = list(next(combinations))
-    best_fps = base_fps
-    for combination in combinations:
-        combination = list(combination)
-        # re-slice the pipeline to handle cases where a suggestion added multiple elements
-        combination = "!".join(combination).split("!")
-        log_parameters_of_interest(combination)
-
-        try:
-            fps = sample_pipeline(combination, sample_duration)
-
-            if fps > best_fps:
-                best_fps = fps
-                best_pipeline = combination
-
-        except Exception as e:
-            logger.debug("Pipeline failed to start: %s", e)
-
-        cur_time = time.time()
-        if cur_time - start_time > search_duration:
-            break
-
-    return best_pipeline, best_fps
 
 def sample_pipeline(pipeline, sample_duration):
     pipeline = pipeline.copy()
@@ -178,35 +172,3 @@ def process_bus(bus):
         else:
             logger.error("Other message: %s", str(message))
         message = bus.pop()
-
-####################################### Utils #####################################################
-
-def prepare_suggestions(pipeline):
-    # Prepare the suggestions structure
-    # Suggestions structure:
-    #   [
-    #       ["element1 param1=value1", "element1 param1=value2", ...other variants],
-    #       ["element2 param1=value1", "element2 param1=value2", ...other variants],
-    #       ["element3 param1=value1", "element3 param1=value2", ...other variants],
-    #       ...other pipeline elements
-    #   ]
-    suggestions = []
-    for element in pipeline:
-        suggestions.append([element])
-    return suggestions
-
-def log_parameters_of_interest(pipeline):
-    for element in pipeline:
-        if "gvadetect" in element:
-            parameters = parse_element_parameters(element)
-            logger.info("Found Gvadetect, device: %s, batch size: %s, nireqs: %s",
-                        parameters.get("device", "not set"),
-                        parameters.get("batch-size", "not set"),
-                        parameters.get("nireq", "not set"))
-
-        if "gvaclassify" in element:
-            parameters = parse_element_parameters(element)
-            logger.info("Found Gvaclassify, device: %s, batch size: %s, nireqs: %s",
-                        parameters.get("device", "not set"),
-                        parameters.get("batch-size", "not set"),
-                        parameters.get("nireq", "not set"))
--- scripts/optimizer/processors/inference.py
+++ scripts/optimizer/processors/inference.py
@@ -1,71 +1,242 @@
 # ==============================================================================
-# Copyright (C) 2025-2025 Intel Corporation
+# Copyright (C) 2025-2026 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 # ==============================================================================
+import logging
 
 from openvino import Core
 
-def add_device_suggestions(suggestions):
-    element_device_suggestions(suggestions, "gvadetect")
-    element_device_suggestions(suggestions, "gvaclassify")
+logger = logging.getLogger(__name__)
 
-def element_device_suggestions(suggestions, element):
-    devices = Core().available_devices
-    for suggestion in suggestions:
-        if element in suggestion[0]:
-            for device in devices:
-                parameters = parse_element_parameters(suggestion[0])
-                if device in parameters.get("device", ""):
-                    continue
+class DeviceGenerator:
+    def __init__(self):
+        self.tracked_elements = []
+        self.devices = []
+        self.pipeline = []
+        self.first_iteration = True
 
-                if "GPU" in device:
-                    parameters["pre-process-backend"] = "va-surface-sharing"
-                    memory = "video/x-raw(memory:VAMemory)"
+    def init_pipeline(self, pipeline):
+        self.tracked_elements = []
+        self.devices = Core().available_devices
+        self.pipeline = pipeline.copy()
+        self.first_iteration = True
 
-                if "NPU" in device:
-                    parameters["pre-process-backend"] = "va"
-                    memory = "video/x-raw(memory:VAMemory)"
+        for idx, element in enumerate(self.pipeline):
+            if "gvadetect" in element or "gvaclassify" in element:
+                self.tracked_elements.append({"index": idx, "device_idx": 0})
 
-                if "CPU" in device:
-                    parameters["pre-process-backend"] = "opencv"
-                    memory = "video/x-raw"
+    def __iter__(self):
+        return self
 
-                parameters["device"] = device
-                suggestion.append(f" vapostproc ! {memory} ! {element} {assemble_parameters(parameters)}")
+    def __next__(self) -> list:
+        # Prepare the next combination of devices
+        end_of_variants = True
+        for element in self.tracked_elements:
+            # Don't change anything on first iteration
+            if self.first_iteration:
+                self.first_iteration = False
+                end_of_variants = False
+                break
 
-def add_batch_suggestions(suggestions):
-    batches = [1, 2, 4, 8, 16, 32]
-    for suggestion in suggestions:
-        for element in ["gvadetect", "gvaclassify"]:
-            if element in suggestion[0]:
-                parameters = parse_element_parameters(suggestion[0])
-                for batch in batches:
-                    parameters["batch-size"] = str(batch)
-                    suggestion.append(f" {element} {assemble_parameters(parameters)}")
+            cur_device_idx = element["device_idx"]
+            next_device_idx = (cur_device_idx + 1) % len(self.devices)
+            element["device_idx"] = next_device_idx
 
+            # Walk through elements while they still
+            # have more device options
+            if next_device_idx > cur_device_idx:
+                end_of_variants = False
+                break
 
-def add_nireq_suggestions(suggestions):
-    nireqs = range(1, 9)
-    for suggestion in suggestions:
-        for element in ["gvadetect", "gvaclassify"]:
-            if element in suggestion[0]:
-                parameters = parse_element_parameters(suggestion[0])
-                for nireq in nireqs:
-                    parameters["nireq"] = str(nireq)
-                    suggestion.append(f" {element} {assemble_parameters(parameters)}")
+        # If all elements have rotated through the entire list
+        # of available devices, then we have run out of variants
+        if end_of_variants:
+            raise StopIteration
+
+        # log device combinations
+        devices = self.tracked_elements.copy()
+        devices = list(map(lambda e: self.devices[e["device_idx"]], devices)) # transform device indices into names
+        logger.info("Testing device combination: %s", str(devices))
+
+        # Prepare pipeline output
+        pipeline = self.pipeline.copy()
+        for element in self.tracked_elements:
+            # Get the pipeline element we're modifying
+            idx = element["index"]
+            (element_type, parameters) = parse_element_parameters(pipeline[idx])
+
+            # Get the device for this element
+            device = self.devices[element["device_idx"]]
+
+            # Configure an appropriate backend and memory location
+            memory = ""
+            if "GPU" in device:
+                parameters["pre-process-backend"] = "va-surface-sharing"
+                memory = "video/x-raw(memory:VAMemory)"
+
+            if "NPU" in device:
+                parameters["pre-process-backend"] = "va"
+                memory = "video/x-raw(memory:VAMemory)"
+
+            if "CPU" in device:
+                parameters["pre-process-backend"] = "opencv"
+                memory = "video/x-raw"
+
+            # Apply current configuration
+            parameters["device"] = device
+            parameters = assemble_parameters(parameters)
+            pipeline[idx] = f" {element_type} {parameters}"
+            pipeline.insert(idx, f" {memory} ")
+            pipeline.insert(idx, " vapostproc ")
+
+        return pipeline
+
+class BatchGenerator:
+    def __init__(self):
+        self.tracked_elements = []
+        self.batches = []
+        self.pipeline = []
+        self.first_iteration = True
+
+    def init_pipeline(self, pipeline):
+        self.tracked_elements = []
+        self.batches = [1, 2, 4, 8, 16, 32]
+        self.pipeline = pipeline.copy()
+        self.first_iteration = True
+
+        for idx, element in enumerate(self.pipeline):
+            if "gvadetect" in element or "gvaclassify" in element:
+                self.tracked_elements.append({"index": idx, "batch_idx": 0})
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> list:
+        # Prepare the next combination of batches
+        end_of_variants = True
+        for element in self.tracked_elements:
+            # Don't change anything on first iteration
+            if self.first_iteration:
+                self.first_iteration = False
+                end_of_variants = False
+                break
+
+            cur_batch_idx = element["batch_idx"]
+            next_batch_idx = (cur_batch_idx + 1) % len(self.batches)
+            element["batch_idx"] = next_batch_idx
+
+            # Walk through elements while they still
+            # have more batch options
+            if next_batch_idx > cur_batch_idx:
+                end_of_variants = False
+                break
+
+        # If all elements have rotated through the entire list
+        # of available batches, then we have run out of variants
+        if end_of_variants:
+            raise StopIteration
+
+        # log device combinations
+        batches = self.tracked_elements.copy()
+        batches = list(map(lambda e: self.batches[e["batch_idx"]], batches)) # transform batch indices into batches
+        logger.info("Testing batch combination: %s", str(batches))
+
+        # Prepare pipeline output
+        pipeline = self.pipeline.copy()
+        for element in self.tracked_elements:
+            # Get the pipeline element we're modifying
+            idx = element["index"]
+            (element_type, parameters) = parse_element_parameters(pipeline[idx])
+
+            # Get the batch for this element
+            batch = self.batches[element["batch_idx"]]
+
+            # Apply current configuration
+            parameters["batch-size"] = str(batch)
+            parameters = assemble_parameters(parameters)
+            pipeline[idx] = f" {element_type} {parameters}"
+
+        return pipeline
+
+class NireqGenerator:
+    def __init__(self):
+        self.tracked_elements = []
+        self.nireqs = []
+        self.pipeline = []
+        self.first_iteration = True
+
+    def init_pipeline(self, pipeline):
+        self.tracked_elements = []
+        self.nireqs = range(1, 9)
+        self.pipeline = pipeline.copy()
+        self.first_iteration = True
+
+        for idx, element in enumerate(self.pipeline):
+            if "gvadetect" in element or "gvaclassify" in element:
+                self.tracked_elements.append({"index": idx, "nireq_idx": 0})
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> list:
+        # Prepare the next combination of nireqs
+        end_of_variants = True
+        for element in self.tracked_elements:
+            # Don't change anything on first iteration
+            if self.first_iteration:
+                self.first_iteration = False
+                end_of_variants = False
+                break
+
+            cur_nireq_idx = element["nireq_idx"]
+            next_nireq_idx = (cur_nireq_idx + 1) % len(self.nireqs)
+            element["nireq_idx"] = next_nireq_idx
+
+            # Walk through elements while they still
+            # have more nireq options
+            if next_nireq_idx > cur_nireq_idx:
+                end_of_variants = False
+                break
+
+        # If all elements have rotated through the entire list
+        # of available nireqs, then we have run out of variants
+        if end_of_variants:
+            raise StopIteration
+
+        # log device combinations
+        nireqs = self.tracked_elements.copy()
+        nireqs = list(map(lambda e: self.nireqs[e["nireq_idx"]], nireqs)) # transform nireq indices into nireqs
+        logger.info("Testing nireq combination: %s", str(nireqs))
+
+        # Prepare pipeline output
+        pipeline = self.pipeline.copy()
+        for element in self.tracked_elements:
+            # Get the pipeline element we're modifying
+            idx = element["index"]
+            (element_type, parameters) = parse_element_parameters(pipeline[idx])
+
+            # Get the nireq for this element
+            nireq = self.nireqs[element["nireq_idx"]]
+
+            # Apply current configuration
+            parameters["nireq"] = str(nireq)
+            parameters = assemble_parameters(parameters)
+            pipeline[idx] = f" {element_type} {parameters}"
+
+        return pipeline
 
 ####################################### Utils #####################################################
 
+# returns element type and parsed parameters
 def parse_element_parameters(element):
     parameters = element.strip().split(" ")
-    del parameters[0]
     parsed_parameters = {}
-    for parameter in parameters:
+    for parameter in parameters[1:]:
         parts = parameter.split("=")
         parsed_parameters[parts[0]] = parts[1]
 
-    return parsed_parameters
+    return (parameters[0], parsed_parameters)
 
 def assemble_parameters(parameters):
     result = ""
